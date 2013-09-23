42cc6b517f05b0bea34b77eccfce20ebc531e1cb
Andy Foy <andy@andyfoyconsulting.co.uk>
2013-09-23 16:28:31 +0100

    Initial commit for master-mapinfo
    Mapinfo version of source code v1.0.1.0 supplied by exeGesIS

--- HLUGISTool/GISApplication/GISAppFactory.cs
+++ HLUGISTool/GISApplication/GISAppFactory.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Windows;
-using HLU.GISApplication.ArcGIS;
 using HLU.GISApplication.MapInfo;
 using HLU.Properties;
-using HLU.UI.View;
-using HLU.UI.ViewModel;
 using Microsoft.Win32;
 
 namespace HLU.GISApplication
@@ -19,49 +16,34 @@ namespace HLU.GISApplication
 
     class GISAppFactory
     {
-        private static Nullable<bool> _arcGisInstalled;
         private static Nullable<bool> _mapInfoInstalled;
-        private static WindowSelectGIS _windowSelGIS;
-        private static ViewModelWindowSelectGIS _viewModelSelGIS;
-        private static GISApplications _gisApp;
-        private static bool _cancelled;
 
         public static GISApp CreateGisApp()
         {
             try
             {
-                _gisApp = GISApplications.None;
+                GISApplications gisApp = GISApplications.None;
 
                 if (Enum.IsDefined(typeof(GISApplications), Settings.Default.PreferredGis))
-                    _gisApp = (GISApplications)Settings.Default.PreferredGis;
+                    gisApp = (GISApplications)Settings.Default.PreferredGis;
 
-                if (_gisApp == GISApplications.None)
+                if (gisApp == GISApplications.None)
                 {
-                    if (ArcGisInstalled && MapInfoInstalled)
+                    if (MapInfoInstalled)
                     {
-                        SelectGISApp();
-                    }
-                    else if (ArcGisInstalled)
-                    {
-                        _gisApp = GISApplications.ArcGIS;
-                    }
-                    else if (MapInfoInstalled)
-                    {
-                        _gisApp = GISApplications.MapInfo;
+                        gisApp = GISApplications.MapInfo;
                     }
 
-                    Settings.Default.PreferredGis = (int)_gisApp;
+                    Settings.Default.PreferredGis = (int)gisApp;
                 }
 
-                if (_gisApp == GISApplications.None)
+                if (gisApp == GISApplications.None)
                     throw new ArgumentException("Could not find GIS application.");
                 else
                     Settings.Default.Save();
 
-                switch (_gisApp)
+                switch (gisApp)
                 {
-                    case GISApplications.ArcGIS:
-                        return new ArcMapApp(Settings.Default.MapPath);
                     case GISApplications.MapInfo:
                         return new MapInfoApp(Settings.Default.MapPath);
                     default:
@@ -70,20 +52,14 @@ namespace HLU.GISApplication
             }
             catch (Exception ex)
             {
-                if (!_cancelled)
-                    MessageBox.Show(ex.Message, "HBIC: Fatal Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show(ex.Message, "HBIC: Fatal Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return null;
             }
         }
 
         public static bool ArcGisInstalled
         {
-            get
-            {
-                if (_arcGisInstalled == null)
-                    _arcGisInstalled = Type.GetTypeFromProgID("esriEditor.editor", false) != null;
-                return (bool)_arcGisInstalled;
-            }
+            get { return false; }
         }
 
         public static bool MapInfoInstalled
@@ -94,30 +70,6 @@ namespace HLU.GISApplication
                     _mapInfoInstalled = Type.GetTypeFromProgID("MapInfo.Application", false) != null;
                 return (bool)_mapInfoInstalled;
             }
-        }
-
-        private static void SelectGISApp()
-        {
-            _windowSelGIS = new WindowSelectGIS();
-            _windowSelGIS.Owner = App.Current.MainWindow;
-            _windowSelGIS.WindowStartupLocation = WindowStartupLocation.CenterScreen;
-
-            _viewModelSelGIS = new ViewModelWindowSelectGIS();
-            _viewModelSelGIS.RequestClose +=
-                new ViewModelWindowSelectGIS.RequestCloseEventHandler(_viewModelSelGIS_RequestClose);
-
-            _windowSelGIS.DataContext = _viewModelSelGIS;
-
-            _windowSelGIS.ShowDialog();
-        }
-
-        private static void _viewModelSelGIS_RequestClose(bool cancelled, GISApplications selectedGISApp)
-        {
-            _viewModelSelGIS.RequestClose -= _viewModelSelGIS_RequestClose;
-            _windowSelGIS.Close();
-
-            _cancelled = cancelled;
-            if (!_cancelled) _gisApp = selectedGISApp;
         }
 
         public static string GetMapPath(GISApplications gisApp)
