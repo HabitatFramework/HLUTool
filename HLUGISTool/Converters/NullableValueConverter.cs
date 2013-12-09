bf37ff94a017fc4b6905c05c7668cee555707de5
Andy Foy <andy@andyfoyconsulting.co.uk>
2013-12-09 14:25:56 +0000

    Display validation errors on options window
    Display validation errors in tooltips when option fields are invalid and
    make integer fields nullable so that they can be validated when null -
    null is actually invalid but no error is shown if they are not nullable.

---
+++ HLUGISTool/Converters/NullableValueConverter.cs
@@ -0,0 +1,44 @@
+﻿// HLUTool is used to view and maintain habitat and land use GIS data.
+// Copyright © 2013 Andy Foy
+// 
+// This file is part of HLUTool.
+// 
+// HLUTool is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// HLUTool is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with HLUTool.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace HLU.Converters
+{
+    class NullableValueConverter : IValueConverter
+    {
+         #region IValueConverter Members
+  
+         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+         {
+             return value;
+         }
+  
+         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+         {
+             if (string.IsNullOrEmpty(value.ToString()))
+                 return null;
+  
+             return value;
+         }
+  
+         #endregion
+    }
+}
