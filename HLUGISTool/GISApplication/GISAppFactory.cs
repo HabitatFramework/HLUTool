3bffc1e62189d368e66fd3b88b40b32a2a1b9b27
Andy Foy <andy@andyfoyconsulting.co.uk>
2014-10-10 10:26:42 +0100

    Add Sql Query fields to options interface
    Add checkbox to use new (advanced) sql query builder and default
    directory to options interface.

--- HLUGISTool/GISApplication/GISAppFactory.cs
+++ HLUGISTool/GISApplication/GISAppFactory.cs
@@ -182,8 +182,7 @@ namespace HLU.GISApplication
                 if (openFileDlg.ShowDialog() == true)
                 {
                     if (File.Exists(openFileDlg.FileName))
-                        Settings.Default.MapPath = openFileDlg.FileName;
-                    return openFileDlg.FileName;
+                        return openFileDlg.FileName;
                 }
             }
             catch { }
