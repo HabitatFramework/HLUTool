ae1ec7c69b252d37cccc5a81b17ebfba99843d43
Andy Foy <andy@andyfoyconsulting.co.uk>
2019-01-22 10:18:46 +0000

    Minor fixes for CR49
    Update comments and copyright statements

--- HLUGISTool/UI/ViewModel/ViewModelWindowMainMerge.cs
+++ HLUGISTool/UI/ViewModel/ViewModelWindowMainMerge.cs
@@ -1,6 +1,7 @@
 ﻿// HLUTool is used to view and maintain habitat and land use GIS data.
 // Copyright © 2011 Hampshire Biodiversity Information Centre
 // Copyright © 2014 Sussex Biodiversity Record Centre
+// Copyright © 2019 London & South East Record Centres (LaSER)
 // 
 // This file is part of HLUTool.
 // 
